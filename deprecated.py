43a0d81edb25ef694318a121f75adc2fc5cf18e6
Arne Sachtler <Arne.Sachtler@dlr.de>
2017-08-24 16:18:04 +0200

    Fix deprecated module for python 3

--- deprecated.py
+++ deprecated.py
@@ -5,11 +5,12 @@ from functools import wraps
 def deprecated(func):
     @wraps(func)
     def new_func(*args, **kwargs):
-        warnings.warn_explicit(
-            "Call to deprecated function {}.".format(func.__name__),
-            category=DeprecationWarning,
-            filename=func.func_code.co_filename,
-            lineno=func.func_code.co_firstlineno + 1
-        )
+        print("Warning: call to deprected function: ".format(func.__name__))
+#        warnings.warn_explicit(
+#            "Call to deprecated function {}.".format(func.__name__),
+#            category=DeprecationWarning,
+#            filename=func.func_code.co_filename,
+#            lineno=func.func_code.co_firstlineno + 1
+#        )
         return func(*args, **kwargs)
     return new_func
